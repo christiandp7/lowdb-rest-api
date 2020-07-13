25237f5e90652296b8477578cae37e554e7a024f
Christian <christiandp7@gmail.com>
2020-07-13 12:05:10 -0400

    Final Commit!!!

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,7 +1,48 @@
+const { getConnection } = require('../database')
+const { v4 } = require('uuid')
+
 const getTasks = (req, res) => {
-  res.send('received')
+  const tasks = getConnection().get('tasks').value();
+  res.json(tasks)
 }
 
+const getTask = (req, res) => {
+  const task = getConnection().get('tasks').find({id: req.params.id}).value();
+  res.json(task)
+  //console.log(req.params.id)
+  //res.json(tasks)
+}
+
+const createTask = (req, res) => {
+  const newTask = {
+    id: v4(),
+    name: req.body.name,
+    description: req.body.description
+  };
+  //console.log(newTask);
+  getConnection().get('tasks').push(newTask).write(); 
+  res.json(newTask);
+
+}
+
+const updateTask = async (req, res) => {
+  const result = await getConnection().get('tasks').find({id: req.params.id})
+        .assign(req.body)
+        .write();
+  res.json(result)
+}
+
+
+const deleteTask = (req, res) => {
+  const result = getConnection().get('tasks').remove({id: req.params.id}).write();
+  res.json(result);
+}
+
+
 module.exports = {
-  getTasks
+  getTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask
 }
--- src/routes/tasks.route.js
+++ src/routes/tasks.route.js
@@ -1,13 +1,13 @@
 const { Router } = require('express')
 const router = Router();
 
-const { getTasks } = require('../controllers/tasks.controller.js')
+const { getTasks, getTask, createTask, updateTask, deleteTask } = require('../controllers/tasks.controller.js')
 
 router.get('/tasks', getTasks)
-router.get('/tasks/:id', )
-router.post('/tasks', )
-router.put('/tasks/:id', )
-router.delete('/tasks/:id', )
+router.get('/tasks/:id', getTask)
+router.post('/tasks', createTask)
+router.put('/tasks/:id', updateTask)
+router.delete('/tasks/:id', deleteTask)
 
 
 
